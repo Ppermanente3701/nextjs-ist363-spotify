5851ee2de43410d06cc6c5b035c036d410452b74
Peter Permanente <ppermanente3701@gmail.com>
2024-05-07 13:26:16 -0400

    my first commit

--- app/page.js
+++ app/page.js
@@ -5,21 +5,9 @@ import styles from "./page.module.css";
 
 const Homepage = () => {
   return (
-    <main>
-      <Heading level={1} marginLeft={6}>
-        Spotify
-      </Heading>
-      <Heading level={2} marginBottom={4}>
-        Artists
-      </Heading>
-      <Heading level={3}>Songs</Heading>
-      <Paragraph intro marginBottom={12}>
-        LLL
-      </Paragraph>
-      <Paragraph marginBottom={1}>LLL</Paragraph>
-      <Paragraph marginBottom={5}>LLL</Paragraph>
-      <Paragraph caption>LLL</Paragraph>
-    </main>
+    <div>
+      <h1>Homepage</h1>
+    </div>
   );
 };
 export default Homepage;
